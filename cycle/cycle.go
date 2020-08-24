9fa73bd25fc7d0458b5234970cae3f985d8c151e
abulo.hoo <abulo.hoo@gmail.com>
2020-08-24 18:53:32 +0800

    add cycle

---
+++ cycle/cycle.go
@@ -0,0 +1,101 @@
+package cycle
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+//Cycle ..
+type Cycle struct {
+	mu      *sync.Mutex
+	wg      *sync.WaitGroup
+	done    chan struct{}
+	quit    chan error
+	closing uint32
+	waiting uint32
+	// works []func() error
+}
+
+//NewCycle new a cycle life
+func NewCycle() *Cycle {
+	return &Cycle{
+		mu:      &sync.Mutex{},
+		wg:      &sync.WaitGroup{},
+		done:    make(chan struct{}),
+		quit:    make(chan error),
+		closing: 0,
+		waiting: 0,
+	}
+}
+
+/*
+// Go ..
+func (c *Cycle) Go(fns ...func() error) {
+	c.works = append(c.works, fns...)
+}
+
+//RunWait ..
+func (c *Cycle) RunWait() <-chan error {
+	go func(c *Cycle) {
+		for _, fn := range c.works {
+			c.wg.Add(1)
+			go func(c *Cycle, fn func() error) {
+				defer c.wg.Done()
+				if err := fn(); err != nil {
+					c.quit <- err
+				}
+			}(c, fn)
+		}
+		c.Wait()
+		close(c.quit)
+	}(c)
+	return c.quit
+}
+*/
+
+//Run a new goroutine
+func (c *Cycle) Run(fn func() error) {
+	c.mu.Lock()
+	//todo add check options panic before waiting
+	defer c.mu.Unlock()
+	c.wg.Add(1)
+	go func(c *Cycle) {
+		defer c.wg.Done()
+		if err := fn(); err != nil {
+			c.quit <- err
+		}
+	}(c)
+}
+
+//Done block and return a chan error
+func (c *Cycle) Done() <-chan struct{} {
+	if atomic.CompareAndSwapUint32(&c.waiting, 0, 1) {
+		go func(c *Cycle) {
+			c.mu.Lock()
+			defer c.mu.Unlock()
+			c.wg.Wait()
+			close(c.done)
+		}(c)
+	}
+	return c.done
+}
+
+//DoneAndClose ..
+func (c *Cycle) DoneAndClose() {
+	<-c.Done()
+	c.Close()
+}
+
+//Close ..
+func (c *Cycle) Close() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if atomic.CompareAndSwapUint32(&c.closing, 0, 1) {
+		close(c.quit)
+	}
+}
+
+// Wait blocked for a life cycle
+func (c *Cycle) Wait() <-chan error {
+	return c.quit
+}
