46bcd527c0c3fceedbcf011ce08551fb813749ac
abulo.hoo <abulo.hoo@gmail.com>
2021-05-08 00:34:55 +0800

    upgrade

--- fmt.sh
+++ fmt.sh
@@ -3,7 +3,7 @@
 path=$(cd `dirname $0`; pwd)
 for file in `ls $path`; do
     if [ -d $path"/"$file ];then
-        if [ "vendor"!=$file ];then
+        if [ "vendor" != $file ];then
             goimports -w $path"/"$file
             gofmt -w $path"/"$file
         fi
