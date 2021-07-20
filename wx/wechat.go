1f4452a836e730bb9476ecd2a91d8b55ce316984
abulo.hoo <abulo.hoo@gmail.com>
2021-07-20 20:00:09 +0800

    add

--- wx/wechat.go
+++ wx/wechat.go
@@ -1,81 +1,84 @@
 package wx
 
 import (
-	"context"
-	"encoding/json"
-	"time"
+	"sync"
 
-	"github.com/abulo/ratel/redis"
 	"github.com/silenceper/wechat/v2"
 	"github.com/silenceper/wechat/v2/miniprogram"
 	programConfig "github.com/silenceper/wechat/v2/miniprogram/config"
 	"github.com/silenceper/wechat/v2/officialaccount"
 	officialConfig "github.com/silenceper/wechat/v2/officialaccount/config"
+	"github.com/silenceper/wechat/v2/openplatform"
+	openConfig "github.com/silenceper/wechat/v2/openplatform/config"
 )
 
 // Client --
 type Client struct {
 	*wechat.Wechat
-	MiniProgram     *miniprogram.MiniProgram
-	OfficialAccount *officialaccount.OfficialAccount
+	miniProgram     map[string]*miniprogram.MiniProgram
+	officialAccount map[string]*officialaccount.OfficialAccount
+	openPlatform    map[string]*openplatform.OpenPlatform
+	mu              sync.RWMutex
 }
 
 func NewClient() *Client {
 	wc := wechat.NewWechat()
-	newClient := &Client{}
+	newClient := &Client{
+		miniProgram:     make(map[string]*miniprogram.MiniProgram),
+		officialAccount: make(map[string]*officialaccount.OfficialAccount),
+		openPlatform:    make(map[string]*openplatform.OpenPlatform),
+	}
 	newClient.Wechat = wc
 	return newClient
 }
 
-// NewMiniProgram 实例化小程序
-func (c *Client) NewMiniProgram(cfg *programConfig.Config) *Client {
+// SetMiniProgram 实例化
+func (c *Client) SetMiniProgram(group string, cfg *programConfig.Config) *Client {
 	clientMiniProgram := c.Wechat.GetMiniProgram(cfg)
-	c.MiniProgram = clientMiniProgram
+	c.mu.Lock()
+	c.miniProgram[group] = clientMiniProgram
+	c.mu.Unlock()
 	return c
 }
 
-func (c *Client) NewOfficialAccount(cfg *officialConfig.Config) *Client {
+// SetOfficialAccount 实例化
+func (c *Client) SetOfficialAccount(group string, cfg *officialConfig.Config) *Client {
 	clientOfficialAccount := c.Wechat.GetOfficialAccount(cfg)
-	c.OfficialAccount = clientOfficialAccount
+	c.mu.Lock()
+	c.officialAccount[group] = clientOfficialAccount
+	c.mu.Unlock()
 	return c
 }
 
-type Cache struct {
-	Driver *redis.Client
+// SetOpenPlatform 实例化
+func (c *Client) SetOpenPlatform(group string, cfg *openConfig.Config) *Client {
+	clientOpenPlatform := c.Wechat.GetOpenPlatform(cfg)
+	c.mu.Lock()
+	c.openPlatform[group] = clientOpenPlatform
+	c.mu.Unlock()
+	return c
 }
 
-//Get 获取一个值
-func (c *Cache) Get(key string) interface{} {
-	ctx := context.TODO()
-	var data interface{}
-	content := c.Driver.Get(ctx, key).Val()
-	json.Unmarshal([]byte(content), &data)
-	return data
+// MiniProgram 获取实例
+func (c *Client) MiniProgram(group string) *miniprogram.MiniProgram {
+	c.mu.RLock()
+	res := c.miniProgram[group]
+	c.mu.RUnlock()
+	return res
 }
 
-//Set 设置一个值
-func (c *Cache) Set(key string, val interface{}, timeout time.Duration) (err error) {
-	ctx := context.TODO()
-	var data []byte
-	if data, err = json.Marshal(val); err != nil {
-		return
-	}
-	_, err = c.Driver.SetEX(ctx, key, string(data), timeout).Result()
-	return
+// OfficialAccount 获取实例
+func (c *Client) OfficialAccount(group string) *officialaccount.OfficialAccount {
+	c.mu.RLock()
+	res := c.officialAccount[group]
+	c.mu.RUnlock()
+	return res
 }
 
-//IsExist 判断key是否存在
-func (c *Cache) IsExist(key string) bool {
-	ctx := context.TODO()
-	a, _ := c.Driver.Exists(ctx, key).Result()
-	return a > 0
-}
-
-//Delete 删除
-func (c *Cache) Delete(key string) error {
-	ctx := context.TODO()
-	if _, err := c.Driver.Del(ctx, key).Result(); err != nil {
-		return err
-	}
-	return nil
+// OfficialAccount 获取实例
+func (c *Client) OpenPlatform(group string) *openplatform.OpenPlatform {
+	c.mu.RLock()
+	res := c.openPlatform[group]
+	c.mu.RUnlock()
+	return res
 }
