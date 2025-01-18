576bb01ac0a7fcc5a62edcd0bb6ca8fd18ce6857
abulo.hoo <abulo.hoo@gmail.com>
2025-01-19 00:15:27 +0800

    config

--- config/config.go
+++ config/config.go
@@ -1,267 +1,93 @@
-/*
-Package config is a go config management implement. support YAML,TOML,JSON,INI,HCL format.
-
-Source code and other details for the project are available at GitHub:
-
-	https://github.com/gookit/config
-
-JSON format content example:
-
-	{
-		"name": "app",
-		"debug": false,
-		"baseKey": "value",
-		"age": 123,
-		"envKey": "${SHELL}",
-		"envKey1": "${NotExist|defValue}",
-		"map1": {
-			"key": "val",
-			"key1": "val1",
-			"key2": "val2"
-		},
-		"arr1": [
-			"val",
-			"val1",
-			"val2"
-		],
-		"lang": {
-			"dir": "res/lang",
-			"defLang": "en",
-			"allowed": {
-				"en": "val",
-				"zh-CN": "val2"
-			}
-		}
-	}
-
-Usage please see example(more example please see examples folder in the lib):
-*/
 package config
 
 import (
 	"fmt"
-	"sync"
+	"os"
+	"path/filepath"
+	"strings"
 
-	"github.com/fsnotify/fsnotify"
+	"github.com/abulo/ratel/v3/util"
+	"github.com/spf13/viper"
 )
 
-// Ini There are supported config format
-const (
-	Ini  = "ini"
-	Hcl  = "hcl"
-	Yml  = "yml"
-	JSON = "json"
-	Yaml = "yaml"
-	Toml = "toml"
-
-	// default delimiter
-	defaultDelimiter byte = '.'
-	// default struct tag name for binding data to struct
-	defaultStructTag = "mapstructure"
-)
-
-// internal vars
-// type intArr []int
-type strArr []string
-
-// type intMap map[string]int
-type strMap map[string]string
-
-// This is a default config manager instance
-var dc = New("default")
-
-// Config structure definition
 type Config struct {
-	// save latest error, will clear after read.
-	err error
-	// config instance name
-	name string
-	lock sync.RWMutex
-
-	// config options
-	opts *Options
-	// all config data
-	data map[string]any
-
-	// loaded config files records
-	loadedFiles []string
-	driverNames []string
-
-	// TODO Deprecated decoder and encoder, use driver instead
-	// drivers map[string]Driver
-
-	// decoders["toml"] = func(blob []byte, v any) (err error){}
-	// decoders["yaml"] = func(blob []byte, v any) (err error){}
-	decoders map[string]Decoder
-	encoders map[string]Encoder
-
-	// cache on got config data
-	intCache map[string]int
-	strCache map[string]string
-	// iArrCache map[string]intArr TODO cache it
-	// iMapCache map[string]intMap
-	sArrCache map[string]strArr
-	sMapCache map[string]strMap
-
-	onConfigChange func(fsnotify.Event)
+	*viper.Viper
 }
 
-// New config instance
-func New(name string) *Config {
+func New() *Config {
 	return &Config{
-		name: name,
-		opts: newDefaultOption(),
-		data: make(map[string]any),
-
-		// default add JSON driver
-		encoders: map[string]Encoder{JSON: JSONEncoder},
-		decoders: map[string]Decoder{JSON: JSONDecoder},
+		Viper: viper.New(),
 	}
 }
 
-// NewEmpty config instance
-func NewEmpty(name string) *Config {
-	return &Config{
-		name: name,
-		opts: newDefaultOption(),
-		data: make(map[string]any),
-
-		// don't add any drivers
-		encoders: map[string]Encoder{},
-		decoders: map[string]Decoder{},
+// LoadFile 加载单个配置文件
+func (c *Config) LoadFile(filePath string) error {
+	// filePath 是完整路径
+	if !util.FileExists(filePath) {
+		return fmt.Errorf("file is not exist: %s", filePath)
 	}
-}
-
-// NewWith create config instance, and you can call some init func
-func NewWith(name string, fn func(c *Config)) *Config {
-	return New(name).With(fn)
-}
-
-// NewWithOptions config instance
-func NewWithOptions(name string, opts ...func(*Options)) *Config {
-	return New(name).WithOptions(opts...)
-}
-
-// Default get the default instance
-func Default() *Config {
-	return dc
-}
-
-/*************************************************************
- * config drivers
- *************************************************************/
-
-// AddDriver set a decoder and encoder driver for a format.
-func AddDriver(driver Driver) { dc.AddDriver(driver) }
-
-// AddDriver set a decoder and encoder driver for a format.
-func (c *Config) AddDriver(driver Driver) {
-	format := driver.Name()
-
-	c.driverNames = append(c.driverNames, format)
-	c.decoders[format] = driver.GetDecoder()
-	c.encoders[format] = driver.GetEncoder()
-}
-
-// HasDecoder has decoder
-func (c *Config) HasDecoder(format string) bool {
-	format = fixFormat(format)
-	_, ok := c.decoders[format]
-	return ok
-}
-
-// HasEncoder has encoder
-func (c *Config) HasEncoder(format string) bool {
-	format = fixFormat(format)
-	_, ok := c.encoders[format]
-	return ok
-}
-
-// DelDriver delete driver of the format
-func (c *Config) DelDriver(format string) {
-	format = fixFormat(format)
-	delete(c.decoders, format)
-	delete(c.encoders, format)
-}
-
-/*************************************************************
- * helper methods
- *************************************************************/
-
-// Name get config name
-func (c *Config) Name() string {
-	return c.name
-}
-
-// Error get last error, will clear after read.
-func (c *Config) Error() error {
-	err := c.err
-	c.err = nil
-	return err
-}
-
-// IsEmpty of the config
-func (c *Config) IsEmpty() bool {
-	return len(c.data) == 0
-}
-
-// LoadedFiles get loaded files name
-func (c *Config) LoadedFiles() []string {
-	return c.loadedFiles
-}
-
-// DriverNames get loaded driver names
-func (c *Config) DriverNames() []string {
-	return c.driverNames
-}
-
-// ClearAll data and caches
-func ClearAll() { dc.ClearAll() }
-
-// ClearAll data and caches
-func (c *Config) ClearAll() {
-	c.ClearData()
-	c.ClearCaches()
-
-	c.loadedFiles = []string{}
-	c.opts.Readonly = false
-}
-
-// ClearData clear data
-func (c *Config) ClearData() {
-	c.fireHook(OnCleanData)
-
-	c.data = make(map[string]any)
-	c.loadedFiles = []string{}
-}
-
-// ClearCaches clear caches
-func (c *Config) ClearCaches() {
-	if c.opts.EnableCache {
-		c.intCache = nil
-		c.strCache = nil
-		c.sMapCache = nil
-		c.sArrCache = nil
+	// 获取文件扩展名
+	ext := filepath.Ext(filePath)
+	if ext == "" {
+		return fmt.Errorf("file extension is required")
 	}
-}
-
-/*************************************************************
- * helper methods
- *************************************************************/
-
-// fire hook
-func (c *Config) fireHook(name string) {
-	if c.opts.HookFunc != nil {
-		c.opts.HookFunc(name, c)
+	// 设置配置文件类型
+	c.SetConfigType(strings.TrimPrefix(ext, "."))
+	// 设置配置文件路径
+	c.SetConfigFile(filePath)
+	// 读取配置文件
+	if err := c.ReadInConfig(); err != nil {
+		return fmt.Errorf("failed to read config file: %w", err)
 	}
+	return nil
 }
 
-// record error
-func (c *Config) addError(err error) {
-	c.err = err
+// LoadDir 加载目录中的所有配置文件
+func (c *Config) LoadDir(dirPath string) error {
+	// 遍历目录中的所有文件
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// 只处理文件
+		if !info.IsDir() {
+			// 获取文件扩展名
+			ext := filepath.Ext(path)
+			if ext == "" {
+				return nil
+			}
+
+			// 设置配置文件类型
+			c.SetConfigType(strings.TrimPrefix(ext, "."))
+
+			// 设置配置文件路径
+			c.SetConfigFile(path)
+
+			// 读取配置文件
+			if err := c.MergeInConfig(); err != nil {
+				return fmt.Errorf("failed to merge config file: %w", err)
+			}
+		}
+
+		return nil
+	})
+
+	if err != nil {
+		return fmt.Errorf("failed to walk directory: %w", err)
+	}
+
+	return nil
 }
 
-// format and record error
-func (c *Config) addErrorf(format string, a ...any) {
-	c.err = fmt.Errorf(format, a...)
+func (c *Config) Exists(key string) bool {
+	return c.IsSet(key)
+}
+
+func (c *Config) BindStruct(key string, data any) error {
+	return c.UnmarshalKey(key, data)
+}
+
+func (c *Config) String(key string) string {
+	return c.GetString(key)
 }
